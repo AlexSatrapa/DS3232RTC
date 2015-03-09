699c6b6266b32c81d9566d3ce7b558a0e943eae3
Alex Satrapa <grail@goldweb.com.au>
2015-03-09 11:31:27 +1100

    Broken, initial attempt at OO migration.

--- DS3232RTC.h
+++ DS3232RTC.h
@@ -1,24 +1,13 @@
-/*
- * DS3232RTC.h - library for DS3232 RTC from Freetronics; http://www.freetronics.com/rtc
- * This library is intended to be used with Arduino Time.h library functions; http://playground.arduino.cc/Code/Time
- * Based on code by Jonathan Oxer at http://www.freetronics.com/pages/rtc-real-time-clock-module-quickstart-guide
- * Based on code by Michael Margolis at http://code.google.com/p/arduino-time
- * Based on code by Ryhs Weather at http://github.com/rweather/arduinolibs/tree/master/libraries/RTC
-
-  Copyright (c) 2013 by the Arduino community (Author anonymous)
-
-  This work is licensed under the Creative Commons ShareAlike 1.0 Generic (CC SA 1.0) License
-  To view a copy of this license, visit http://creativecommons.org/licenses/sa/1.0 or send a
-  letter to Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- */
-
 #ifndef DS3232RTC_h
 #define DS3232RTC_h
 
+#if ARDUINO >= 100
+#include <Arduino.h>
+#else
+#include <WProgram.h>
+#endif
+
+#include <Time.h>
 #include <RTC.h>
 
 // Based on page 11 of specs; http://www.maxim-ic.com/datasheet/index.mvp/id/4984
@@ -31,51 +20,16 @@
 /**
  * DS3232RTC Class
  */
-class DS3232RTC
+class DS3232RTC: public DSRTC
 {
   public:
     DS3232RTC();
-    static bool available();
-    // Date and Time
-    static time_t get();
-    static void set(time_t t);
-    static void read(tmElements_t &tm);
-    static void write(tmElements_t &tm);
-    static void writeTime(tmElements_t &tm);
-    static void writeDate(tmElements_t &tm);
-    // Alarms
-    static void readAlarm(uint8_t alarm, alarmMode_t &mode, tmElements_t &tm);
-    static void writeAlarm(uint8_t alarm, alarmMode_t mode, tmElements_t tm);
-    // Control Register
-    static void setBBOscillator(bool enable);
-    static void setBBSqareWave(bool enable);
-    static void setSQIMode(sqiMode_t mode);
-    static bool isAlarmInterrupt(uint8_t alarm);
-    static uint8_t readControlRegister();
-    static uint8_t readStatusRegister();
-    // Control/Status Register
-    static bool isOscillatorStopFlag();
-    static void setOscillatorStopFlag(bool enable);
-    static void setBB33kHzOutput(bool enable);
-    static void setTCXORate(tempScanRate_t rate);
-    static void set33kHzOutput(bool enable);
-    static bool isTCXOBusy();
-    static bool isAlarmFlag(uint8_t alarm);
-    static uint8_t isAlarmFlag();
-    static void clearAlarmFlag(uint8_t alarm);
-    // Temperature
-    static void readTemperature(tpElements_t &tmp);
-  private:
-    static uint8_t dec2bcd(uint8_t num);
-    static uint8_t bcd2dec(uint8_t num);
   protected:
-    static void _wTime(tmElements_t &tm);
-    static void _wDate(tmElements_t &tm);
-    static uint8_t read1(uint8_t addr);
-    static void write1(uint8_t addr, uint8_t data);
+    uint8_t read1(uint8_t addr);
+    void write1(uint8_t addr, uint8_t data);
+    void readN(uint8_t addr, uint8_t buf[], uint8_t len);
+    void writeN(uint8_t addr, uint8_t buf[], uint8_t len);
 };
-
-extern DS3232RTC RTC;
 
 /**
  * DS3232SRAM Class
