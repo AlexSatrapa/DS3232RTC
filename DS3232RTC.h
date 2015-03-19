d655c5593fa3c6978a0b3731c21cacb47df21ae8
Alex Satrapa <grail@goldweb.com.au>
2015-03-19 22:45:40 +1100

    Convert to subclass of DSRTC.

--- DS3232RTC.h
+++ DS3232RTC.h
@@ -31,52 +31,16 @@
 /**
  * DS3232RTC Class
  */
-class DS3232RTC
+class DS3232RTC: public DSRTC
 {
   public:
     DS3232RTC();
     bool available();
-    // Date and Time
-    time_t get();
-    void set(time_t t);
-    void read(tmElements_t &tm);
-    void write(tmElements_t &tm);
-    void writeTime(tmElements_t &tm);
-    void writeDate(tmElements_t &tm);
-    // Alarms
-    void readAlarm(uint8_t alarm, alarmMode_t &mode, tmElements_t &tm);
-    void writeAlarm(uint8_t alarm, alarmMode_t mode, tmElements_t tm);
-    // Control Register
-    void setBBOscillator(bool enable);
-    void setBBSqareWave(bool enable);
-    void setSQIMode(sqiMode_t mode);
-    bool isAlarmInterrupt(uint8_t alarm);
-    uint8_t readControlRegister();
-    void writeControlRegister(uint8_t value);
-    // Control/Status Register
-    bool isOscillatorStopFlag();
-    void setOscillatorStopFlag(bool enable);
-    void setBB33kHzOutput(bool enable);
-    void setTCXORate(tempScanRate_t rate);
-    void set33kHzOutput(bool enable);
-    bool isTCXOBusy();
-    bool isAlarmFlag(uint8_t alarm);
-    uint8_t isAlarmFlag();
-    void clearAlarmFlag(uint8_t alarm);
-    uint8_t readStatusRegister();
-    void writeStatusRegister(uint8_t value);
-    // Temperature
-    void readTemperature(tpElements_t &tmp);
-  private:
-    uint8_t dec2bcd(uint8_t num);
-    uint8_t bcd2dec(uint8_t num);
   protected:
     uint8_t read1(uint8_t addr);
     void write1(uint8_t addr, uint8_t data);
     void readN(uint8_t addr, uint8_t buf[], uint8_t len);
     void writeN(uint8_t addr, uint8_t buf[], uint8_t len);
-  	void populateTimeElements( tmElements_t &tm, uint8_t TimeDate[] );
-    void populateDateElements( tmElements_t &tm, uint8_t TimeDate[] );
 };
 
 extern DS3232RTC RTC;
