491fda866204dbca3198ee5a6a0562bfc6824cda
Alex Satrapa <grail@goldweb.com.au>
2015-03-28 17:01:47 +1100

    Remove SRAM class, deal with it later.

--- DS3232.h
+++ DS3232.h
@@ -20,6 +20,7 @@
 #define DS3232_h
 
 #include <DSRTC.h>
+#include <Wire.h>
 
 // Based on page 11 of specs; http://www.maxim-ic.com/datasheet/index.mvp/id/4984
 #define DS3232_I2C_ADDRESS 0x68
@@ -42,45 +43,5 @@ class DS3232: public DSRTC
     void readN(uint8_t addr, uint8_t buf[], uint8_t len);
     void writeN(uint8_t addr, uint8_t buf[], uint8_t len);
 };
-
-extern DS3232 RTC;
-
-/**
- * DS3232SRAM Class
- */
-class DS3232SRAM : public Stream
-{
-  public:
-    DS3232SRAM();
-    // more like EEPROMClass
-    uint8_t read(int addr);
-    void write(int addr, uint8_t data);
-
-    // from Print class
-    #if ARDUINO >= 100
-    virtual size_t write(uint8_t data);
-    virtual size_t write(const char *str);
-    virtual size_t write(const uint8_t *buf, size_t size);
-    #else
-    virtual void write(uint8_t);
-    virtual void write(const char *str);
-    virtual void write(const uint8_t *buf, size_t size);
-    #endif
-
-    // from Stream class
-    virtual int available();
-    virtual int read();
-    virtual int peek();
-    virtual void flush();  // Sets the next character position to 0.
-    uint8_t seek(uint8_t pos);  // Sets the position of the next character to be extracted from the stream.
-    uint8_t tell();  // Returns the position of the current character in the stream.
-
-  private:
-    bool _init;
-    bool _avail;
-    uint8_t _cursor;
-};
-
-extern DS3232SRAM SRAM;
 
 #endif
